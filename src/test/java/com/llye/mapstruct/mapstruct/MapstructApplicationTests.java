a84aa64dff68595522067aa0e63f9ec3d8281742
joycelye <joycelye1991@gmail.com>
2023-08-26 21:10:37 +0800

    init

---
+++ src/test/java/com/llye/mapstruct/mapstruct/MapstructApplicationTests.java
@@ -0,0 +1,13 @@
+package com.llye.mapstruct.mapstruct;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
